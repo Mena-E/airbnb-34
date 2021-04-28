e0e05e244a92b5cf13ceaa2aea11efb117ba96d6
Mena-E <odemena.ekelemu@gmail.com>
2021-04-28 17:42:29 -0400

    Modifie predictions.py file

--- pages/predictions.py
+++ pages/predictions.py
@@ -2,11 +2,18 @@
 import dash
 import dash_bootstrap_components as dbc
 import dash_core_components as dcc
+import dash_daq as daq
 import dash_html_components as html
 from dash.dependencies import Input, Output
+import pandas as pd
+from joblib import load
 
 # Imports from this application
 from app import app
+
+model = load('assets/model.joblib')
+
+
 
 # 2 column layout. 1st column width = 4/12
 # https://dash-bootstrap-components.opensource.faculty.ai/l/components/layout
@@ -14,21 +21,71 @@ column1 = dbc.Col(
     [
         dcc.Markdown(
             """
-        
-            ## Predictions
-
-            Your instructions: How to use your app to get new predictions.
+            ## Select your Features
 
             """
         ),
+        dcc.Markdown('##### Number of Bathrooms'),
+            dcc.Slider(id='Bathrooms',
+                    min=0,
+                    max=15.5,
+                    step=0.5,
+                    value=1),
+            dcc.Markdown('',id='Output_Bath',
+                        style={'textAlign':'center',
+                                'font-size':15}), 
+
+            dcc.Markdown('##### Number of Bedrooms'),
+            dcc.Slider(id='Bedrooms',
+                    min=0,
+                    max=21,
+                    step=1,
+                    value=1),
+            dcc.Markdown('',id='Output_Bathroom',
+                        style={'textAlign':'center',
+                                'font-size':15})
+    
     ],
-    md=4,
+    md=6,
 )
 
 column2 = dbc.Col(
     [
-
+        dcc.Markdown('### Predicted Rental price ($)'),
+        daq.LEDDisplay(
+            id='prediction-content',
+            size=25,
+            color="#42f55d"),
+            html.Img(src='assets/airbnb_pic2.jpg', className='img-fluid')
     ]
 )
+
+
+@app.callback(
+    Output(component_id='Output_Bathrooms', component_property='children'),
+    [Input(component_id='Bathrooms', component_property='value')])
+def update_output_div(input_value):
+    return 'You have selected: {} bathrooms'.format(input_value)
+
+
+@app.callback(
+    Output(component_id='Output_Bedrooms', component_property='children'),
+    [Input(component_id='Bedrooms', component_property='value')])
+def update_output_div2(input_value):
+    return 'You have selected: {} bedrooms'.format(input_value)
+
+
+@app.callback(
+    Output('prediction-content','value'),
+    [ Input('Bathrooms', 'value'),
+      Input('Bedrooms', 'value')
+     ])
+
+def predict(bathrooms, bedrooms):
+    df = pd.DataFrame(columns=['bathrooms','bedrooms'],
+    data=[[bathrooms, bedrooms]])
+    y_pred = model.predict(df)[0]
+    result = round(y_pred, 2)
+    return result
 
 layout = dbc.Row([column1, column2])
